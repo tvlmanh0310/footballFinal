bd69ee5d31b6965a8c69cf74fec5febcc286567f
tvlmanh0310 <144315988+tvlmanh0310@users.noreply.github.com>
2024-01-13 18:31:08 +0700

    Create DeleteTeam.java

---
+++ src/main/java/Controller/DeleteTeam.java
@@ -0,0 +1,48 @@
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import DBContext.TeamDBContext;
+
+/**
+ * Servlet implementation class DeleteTeam
+ */
+@WebServlet("/DeleteTeam")
+public class DeleteTeam extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteTeam() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String TeamID = request.getParameter("teamID");
+		if (TeamID != null && !TeamID.isEmpty()) {
+			TeamDBContext teamDBContext = new TeamDBContext();
+			teamDBContext.deleteTeam(TeamID);
+		}
+		response.sendRedirect("TeamServlet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
