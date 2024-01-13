fa0dd80bc3bfc585b4c86b2bca3bd32657fe6fe0
tvlmanh0310 <144315988+tvlmanh0310@users.noreply.github.com>
2024-01-13 18:26:46 +0700

    Create TeamServlet.java

---
+++ src/main/java/com/example/servlets/TeamServlet.java
@@ -0,0 +1,55 @@
+package com.example.servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.example.models.Team;
+import DBContext.TeamDBContext;
+
+/**
+ * Servlet implementation class TeamServlet
+ */
+@WebServlet("/TeamServlet")
+public class TeamServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TeamServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			TeamDBContext TeamDBConetxt = new TeamDBContext();
+			try {
+				List<Team> list = TeamDBConetxt.getAllTeams();
+				System.out.println(list);
+
+				request.setAttribute("ListTeams", list);
+				request.getRequestDispatcher("Team.jsp").forward(request, response);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
